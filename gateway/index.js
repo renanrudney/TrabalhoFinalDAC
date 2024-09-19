2529d4bd45fa2268e084a16ca072e76bc70af9c2
Renan Rudney <renanrudney@gmail.com>
2024-09-19 16:29:47 -0300

    Adicionado código base do gateway

---
+++ gateway/index.js
@@ -0,0 +1,48 @@
+require('dotenv-safe').config()
+const jwt = require('jsonwebtoken')
+var http = require('http')
+const express = require('express')
+const httpProxy = require('express-http-proxy')
+const app = express()
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// TODO: JWT Logic
+function verifyJWT(req, res, next) {
+  next();
+}
+
+// proxies
+// TODO: Add proxies
+// const exampleProxy = httpProxy('http://localhost:5002')
+
+// routes
+// TODO: Add routes
+// app.get('/example', function (req, res, next) {
+//   exampleProxy(req, res, next)
+// })
+
+// config
+app.use(logger('dev'))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// server
+var server = http.createServer(app)
+server.listen(3000)
+console.log(`Server listening on port 3000!`)
