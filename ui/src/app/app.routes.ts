80db70ded7583a2579e999d9622099b65c7ff9ea
maricotnicolas <nicolasmaricot@yahoo.fr>
2024-09-19 20:52:54 -0300

    autocadastro

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+//implements {AutocadastroComponent}
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'autocadastro', component: AutocadastroComponent },
+    { path: '', redirectTo: '/autocadastro', pathMatch: 'full' }
+];
