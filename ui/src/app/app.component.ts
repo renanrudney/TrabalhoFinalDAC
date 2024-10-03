08e15e1e7a7c5d8949fd7cf3cc5e87c223cf7792
maricotnicolas <nicolasmaricot@yahoo.fr>
2024-10-03 07:19:19 -0300

    R03 - Pagina inciacial cliente criada

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { InicialClienteComponent } from './pages/inicial-cliente';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [InicialClienteComponent],
+  template: '<app-tela-inicial-cliente></app-tela-inicial-cliente>',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ui';
-}
+export class AppComponent {}
--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './autocadastro/autocadastro.component';
 //implements {AutocadastroComponent}
+import { InicialClienteComponent } from './pages/inicial-cliente';
 
 export const routes: Routes = [
 
     { path: 'autocadastro', component: AutocadastroComponent },
+    { path: 'inicial-cliente', component: InicialClienteComponent },
     { path: '', redirectTo: '/autocadastro', pathMatch: 'full' }
 ];
