6ed3d7edb0f8d82d0092fed447602b491727847c
Renan Rudney <renanrudney@gmail.com>
2024-10-30 23:12:08 -0300

    feat: create RabbitMQ Orchestrator

---
+++ orchestrator/src/main/java/br/ufpr/dac/orchestrator/connections/RabbitMQConnection.java
@@ -0,0 +1,50 @@
+package br.ufpr.dac.orchestrator.connections;
+
+import br.ufpr.dac.common.constants.RabbitMQConstants;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Binding.DestinationType;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class RabbitMQConnection {
+  private static final String NOME_EXCHANGE = "amq.direct";
+  private AmqpAdmin amqpAdmin;
+
+  public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+    this.amqpAdmin = amqpAdmin;
+  }
+
+  private Queue novaFila(String nomeFila) {
+		return new Queue(nomeFila, true, false, false);
+	}
+	
+	private DirectExchange novaExchange() {
+		return new DirectExchange(NOME_EXCHANGE);
+	}
+	
+	private Binding relacionarFilaExchange(Queue fila, DirectExchange exchange) {
+		return new Binding(fila.getName(), DestinationType.QUEUE, exchange.getName(),
+				fila.getName(), null);
+	}
+
+  @PostConstruct
+	private void criarFilas() {
+		DirectExchange exchange = novaExchange();
+		this.amqpAdmin.declareExchange(exchange);
+
+		String[] nomeFilas = RabbitMQConstants.TODAS_FILAS;
+
+		for (String nomeFila : nomeFilas) {
+			Queue fila = this.novaFila(nomeFila);
+			Binding binding = this.relacionarFilaExchange(fila, exchange);
+			this.amqpAdmin.declareQueue(fila);
+			this.amqpAdmin.declareBinding(binding);
+		}
+	}
+}
