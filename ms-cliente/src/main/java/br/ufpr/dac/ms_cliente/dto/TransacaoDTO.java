c8213fa5e01253889e94c920973d07b949ec06d8
Renan Rudney <renanrudney@gmail.com>
2024-12-10 21:03:38 -0300

    refactor: ms-cliente

--- ms-cliente/src/main/java/br/ufpr/dac/ms_cliente/dto/TransacaoDTO.java
+++ ms-cliente/src/main/java/br/ufpr/dac/ms_cliente/dto/TransacaoDTO.java
@@ -1,6 +1,7 @@
 package br.ufpr.dac.ms_cliente.dto;
 
 import java.util.Date;
+import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TransacaoDTO {
   private Long id;
-  private String idCliente;
+  private UUID idCliente;
   private Date dataHora;
   private Double qtdMilhas;
   private boolean entrada;
